3f68438bd90b9b971277ba0e1ccc2a83fa8a36f9
Ankita <ankitarattan@yahoo.com>
2022-07-08 00:22:14 +0530

    files with changes

---
+++ src/main/java/Program/base.java
@@ -0,0 +1,26 @@
+package Program;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class base {
+	//public WebDriver driver;
+
+@SuppressWarnings("deprecation")
+public WebDriver InitlializeDriver() throws IOException
+{
+	Properties prop= new Properties();
+	FileInputStream fis = new FileInputStream("src\\main\\java\\Program\\data.properties");
+	prop.load(fis);
+    System.setProperty(prop.getProperty("chromeDriver"), "driver/chromedriver.exe");
+    	//chrome driver create folder here
+		WebDriver driver= new ChromeDriver();
+		return driver;	
+ 
+}
+}
